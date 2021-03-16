b4816ae933b3f222513f882dcf0de774bad2113a
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-03-16 20:20:58 +0200

    experiments

--- .github/actions/my-action/index.js
+++ .github/actions/my-action/index.js
@@ -1,1 +1,13 @@
+const path = require("path");
+const fs   = require("fs");
+let files  = [];
+
+function throughDirectory(dir) {
+    fs.readdirSync(dir).forEach(file => {
+        const absolute = path.join(dir, file);
+        if (fs.statSync(absolute).isDirectory()) return throughDirectory(absolute);
+        else return files.push(absolute);
+    });
+}
 console.log('HELLO WORLD');
+console.log(process.env);
